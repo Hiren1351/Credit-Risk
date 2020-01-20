8b49dbb911009a7b3d73fba733338896afd30646
Hiren Shah <59893634+Hiren1351@users.noreply.github.com>
2020-01-20 13:21:39 +0530

    Update Logistic_Regression for Credit Risk.py

--- Logistic_Regression for Credit Risk.py
+++ Logistic_Regression for Credit Risk.py
@@ -8,7 +8,7 @@ Created on Sat Jan 18 16:47:51 2020
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
+
 
 loan_data = pd.read_csv('01Exercise1.csv')
 
@@ -28,20 +28,20 @@ loan['gender'].mode()
 
 gender_null = loan[loan['gender'].isnull()].index.tolist()
 
-loan['gender'].iloc[gender_null] = 'Male'
+loan['gender'].iloc[gender_null] = 'Male' #We fill missing value in gender by mode
 
 loan.isnull().sum(axis = 0)
 
-#Fill the missing Value in ch Column
+#Fill the missing Value in credit History Column
 
-Counter(loan['ch'])#there is 475 1 and 89 0 so we replace with 1
+Counter(loan['ch'])#there is 475 1 and 89 0 so majority is 1 but still we want take any kind of risk because credit history is sensetive
 loan['ch'].mode()
 
-pd.crosstab(loan['ch'].isnull(),loan['status'])
+pd.crosstab(loan['ch'].isnull(),loan['status'])#we compare status(y-predicted or outcome variable) based on credit history
 
 #We can see that there is 37 missing in yes and 13 missing in no
 ch_null = loan[(loan['status'] == 'Y') & (loan['ch'].isnull())].index.tolist()
-loan['ch'].iloc[ch_null] = 1
+loan['ch'].iloc[ch_null] = 1 #out of 50 missing value 37 is from 1 so we simply replace with 1
 
 ch_nulll = loan[(loan['status'] == 'N') & (loan['ch'].isnull())].index.tolist()
 loan['ch'].iloc[ch_nulll] = 0
@@ -51,15 +51,15 @@ loan['ch'].iloc[ch_nulll] = 0
 loan['loanamt'].mean()
 
 loanamt_null = loan[loan['loanamt'].isnull()].index.tolist() 
-loan['loanamt'].iloc[loanamt_null] = 146 
+loan['loanamt'].iloc[loanamt_null] = 146 #replace with mean
 
-#married column has only 3 missing value so we dropna
+#married column has only 3 missing value so we drop
 
 loan = loan.dropna()
 
 #ch is a float but there is only two value 0 and 1 so we dont need to convert it
 
-#Gender column is not impact much more for any bank to descide give loan or not so drop it
+#Gender column is not impact much more for any bank to decide that give loan or not based on gender so drop it
 
 loan = loan.drop(['gender'],axis = 1)
 
@@ -80,10 +80,10 @@ loan['loanamt'] = scaler.fit_transform(loan[['loanamt']])
 X = loan.drop(['status_Y'],axis = 1)
 Y = loan[['status_Y']]
 
-#Split tha datainto Train and Test 
+#Split tha data into Train and Test 
 
 from sklearn.model_selection import train_test_split
-X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size = 0.3,random_state = 1234,stratify = Y)
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size = 0.3,random_state = 1234,stratify = Y)#we used stratified sampling 
 
 
 #Build the model
@@ -98,13 +98,13 @@ from sklearn.metrics import classification_report,confusion_matrix
 cr1 = classification_report(Y_test,Pred)
 cm1 = confusion_matrix(Y_test,Pred)
 
-#out of 184 record model predict 36 incorrect in 34 is false positive that we want to descrese
+#out of 184 record model predict 36 incorrect in which 34 is false positive that we want to descrese
 
 #Create prediction Probability for checking threshold for sucess and failure
 
-Y_prob = Lg.predict_proba(X_test)[:,1] #we check what %threshold for negative or 0
+Y_prob = Lg.predict_proba(X_test)[:,1] #we check what %threshold for positive or 1
 
-#lets set threshold 0.70% for above 80% predict positive or approve loan and below 70% reject for loan
+#lets set different threshold 
 
 #Threshold at 80%
 Y_pred_new = []
@@ -120,7 +120,7 @@ cr2 = classification_report(Y_test,Y_pred_new)
 cm2 = confusion_matrix(Y_test,Y_pred_new)
 
 #We can see when we set threshold 80% than true positive rate descrese as well as false nagative also increase
-#out of 184 record model predict 57 incorrect in 13 is false positive and 44 False Nagative that we want to descrese
+#out of 184 record model predict 57 incorrect in which 13 is false positive and 44 False Nagative that we want to descrese
 #so lets change the threshold again from 80% to 78.5% and 60%
 
 #Threshold at 78.5%
@@ -136,7 +136,7 @@ for i in range(0,len(Y_prob)):
 cr3 = classification_report(Y_test,Y_pred_new1)
 cm3 = confusion_matrix(Y_test,Y_pred_new1)
 
-#out of 184 record model predict 31 incorrect in 26 is false positive and 05F alse Nagative that we want to descrese
+#out of 184 record model predict 31 incorrect in which 26 is false positive and 05 False Nagative that we want to descrese
 
 
 #Threshold at 60%
@@ -152,13 +152,13 @@ for i in range(0,len(Y_prob)):
 cr4 = classification_report(Y_test,Y_pred_new2)
 cm4 = confusion_matrix(Y_test,Y_pred_new2)
 
-#out of 184 record model predict 36 incorrect in 34 is false positive and 02 F alse Nagative that we want to descrese
+#out of 184 record model predict 36 incorrect in which 34 is false positive and 02 False Nagative that we want to descrese
 
 #So,In Conclusion we can say that above 80% will aprove and below 60% reject
 #Anything between 60-80 Hold
 
-"""At 80% - so total 95 approve out of total 184
-   At 60% - so total 26 Reject out of total 184
+"""At 80% - total 95 approve out of 184
+   At 60% - total 26 Reject out of 184
    Between 60-80% - Remaining is 63 on Hold or we can say Manual check"""
    
    
